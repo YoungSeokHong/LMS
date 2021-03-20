89f92b98d75f99de258bdacbef78f9dc0e99918f
YoungSeok Hong <cevinlion@gmail.com>
2021-03-21 01:36:42 +0900

    Start a LMS project

---
+++ app/src/main/java/com/example/lms/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_signIn;
+    private TextView tv_sinUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_signIn = findViewById(R.id.btn_signIn);
+        tv_sinUp = findViewById(R.id.tv_sinUp);
+        btn_signIn.setOnClickListener(signInListener);
+    }
+
+    View.OnClickListener signInListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    };
+
+}
