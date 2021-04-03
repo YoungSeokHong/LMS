ff768484c701df67bb083c0712fcdddf7dafe9fc
YoungSeok Hong <cevinlion@gmail.com>
2021-04-03 19:28:12 +0900

    Done SingUp xml

--- app/src/main/java/com/example/lms/SignUpActivity.java
+++ app/src/main/java/com/example/lms/SignUpActivity.java
@@ -2,8 +2,11 @@ package com.example.lms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -12,8 +15,10 @@ public class SignUpActivity extends AppCompatActivity {
 
     public ArrayList collegeList = new ArrayList<>();
     public ArrayList majorList = new ArrayList<>();
-    Spinner spn_college;
-    Spinner spn_major;
+    private Spinner spn_college;
+    private Spinner spn_major;
+    private Button btn_cancel;
+    private Button btn_ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,8 @@ public class SignUpActivity extends AppCompatActivity {
 
         spn_college = (Spinner)findViewById(R.id.spn_college);
         spn_major = (Spinner)findViewById(R.id.spn_major);
+        btn_ok = (Button)findViewById(R.id.btn_ok);
+        btn_cancel = (Button)findViewById(R.id.btn_cancel);
         collegeList.add("경영대학");
         collegeList.add("ICT융합대학");
         collegeList.add("법과대학");
@@ -33,10 +40,24 @@ public class SignUpActivity extends AppCompatActivity {
         majorList.add("국어국문학과");
         majorList.add("중어중문학과");
         ArrayAdapter collegeArrayAdapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,collegeList);
-        spn_college.setAdapter(collegeArrayAdapter);
         ArrayAdapter majorArrayAdapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,majorList);
+        spn_college.setAdapter(collegeArrayAdapter);
         spn_major.setAdapter(majorArrayAdapter);
-
-
+        btn_ok.setOnClickListener(okListener);
+        btn_cancel.setOnClickListener(cancelListener);
     }
+
+    View.OnClickListener okListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+    View.OnClickListener cancelListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    };
 }
